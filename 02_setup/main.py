cb09cb61dca4e47f80c59ed88545fa0a82fc6083
gucci1909 <umangarora0134@gmail.com>
2025-08-26 05:52:07 +0530

    python course

--- 02_setup/main.py
+++ 02_setup/main.py
@@ -1,1 +1,19 @@
 print("Hello, Python!")
+print("this is a test")
+
+a = 10
+
+if a < 5:
+    print("a is less than 5")
+elif a == 5:
+    print("a is equal to 5")
+else:
+    print("a is greater than or equal to 5")
+
+
+# if(a <5):
+#     print("a is less than 5")
+# elif(a == 5):
+#     print("a is equal to 5")
+# else:
+#     print("a is greater than or equal to 5")
