9ad47c67d5bcd57d12a294212ee3e24a0c726ea6
gucci1909 <umangarora0134@gmail.com>
2025-08-26 06:09:51 +0530

    types added

---
+++ 03_setup/main.py
@@ -0,0 +1,21 @@
+age = 10;
+name = "John";
+
+cgpa = 3.5;
+is_student = True;
+
+boolean_value = False;
+
+print("Age:", age);
+print("Name:", name);
+print("CGPA:", cgpa);
+print("Is Student:", is_student);
+print("Boolean Value:", boolean_value);
+print("Type of age:", type(age));
+print("Type of name:", type(name));
+print("Type of cgpa:", type(cgpa));
+# list tuple dictionary set
+my_list = [1, 2, 3, 4, 5];
+my_tuple = (1, 2, 3, 4, 5);
+my_dict = {"name": "John", "age": 10};
+my_set = {1, 2, 3, 4, 5};
