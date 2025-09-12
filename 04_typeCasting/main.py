cd81db63c153f51a39b64b9b1464cffa410270a4
gucci1909 <umangarora0134@gmail.com>
2025-09-12 05:34:48 +0530

    python series:'

--- 04_typeCasting/main.py
+++ 04_typeCasting/main.py
@@ -4,3 +4,6 @@ b = "10";
 c = a + int(b);  # Type casting b to integer
 print("Sum of a and b after type casting:", c);
 print("Type of c:", type(c));
+
+d = str(a) + b;  # Type casting a to string
+print("Concatenation of a and b after type casting:", d);
